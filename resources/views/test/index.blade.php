1c7f6427698edf8ff4f4e41f09c4b65a3d4e34cd
vicha <pongloverponglover@gmail.com>
2021-07-09 12:28:37 +0700

    First Push to Heroku

---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,22 @@
+<html>
+
+<body>
+
+    <h1>gallery of 62122420013 วิชชา สมานจิต</h1>
+    <p1>ant<p1>
+    <a href="galleryant" > <img src="{{$ant}}"  width="300" height="200" > </a>
+    <p1>bird<p1>
+    <img src="{{$bird}}" width="300" height="200" >
+    <p1>cat<p1>
+    <img src="{{$cat}}" width="300" height="200" >
+    <p1>god<p1>
+    <img src="{{$god}}" width="300" height="200" >
+    <p1>spider<p1>
+    <img src="{{$spider}}" width="300" height="200" >
+    
+    
+
+    
+</body>
+
+</html>
---
+++ resources/views/test/indexant.blade.php
@@ -0,0 +1,12 @@
+<html>
+
+<body>
+
+    <h1>Ant of gallery  62122420013 วิชชา สมานจิต</h1>
+    <p1>ant<p1>
+    <img src="{{$ant}}" width="300" height="200">
+    
+    
+</body>
+
+</html>
