50ba236210333be07a60e8d18b4404371abe16ef
vicha <pongloverponglover@gmail.com>
2021-08-20 12:00:58 +0700

    ep9

--- routes/web.php
+++ routes/web.php
@@ -109,3 +109,6 @@ Route::get('/staff/{id}',[ StaffController::class,'show' ]);
 Route::post("/staff",[ StaffController::class , "store" ]);
 Route::patch("/staff/{id}", [ StaffController::class , "update" ]);
 Route::delete('/staff/{id}', [ StaffController::class , 'destroy' ]);
+
+use App\Http\Controllers\PostController;
+Route::resource('post', PostController::class);
